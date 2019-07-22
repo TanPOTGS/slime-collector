0c372cbca752df0d932cf426970980f6aaf24bcc
TanPOTGS <paulroque13@gmail.com>
2019-07-21 19:40:28 -0500

    Stripped react app of files and style I didn't need. It is ready to go

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+
+const app = express();
+
+require('dotenv').config();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(favicon(path.join(__dirname, 'build', 'paulicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+//This is a "catch all" route. No matter the GET request,
+//index.html will be served. Once served, react will use 
+//client-side routing for any request.
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+  console.log(`Hey! Your Express app is running on port ${port}!`);
+});
