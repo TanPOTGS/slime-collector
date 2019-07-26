f5ab5ff07a1ac55f04ad3691327d5bd2dea450a5
TanPOTGS <paulroque13@gmail.com>
2019-07-26 15:45:04 -0500

    The error msg functionality is currently working. Added token services file, unique property to the player schema, and useCreateIndex to database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useCreateIndex: true});
 
 const db = mongoose.connection;
 
 db.once('connected', () => {
-  console.log(`Hey! You're connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
+  console.log(`You're also connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
 });
--- models/player.js
+++ models/player.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 const SALT_ROUNDS = 7;
 
 const playerSchema = new mongoose.Schema({
-  username: {type: String, required: true},
+  username: {type: String, required: true, unique: true},
   password: String
 }, {
   timestamps: true
