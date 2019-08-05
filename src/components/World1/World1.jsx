b90720522430340afbab1fc9fa3c8ee95611959f
TanPOTGS <paulroque13@gmail.com>
2019-08-05 18:51:20 -0500

    Added state to world 1 component. This state holds the player's x and y positions. Also added two lifecycle methods that handle the key down event listeners. State is being updated when a key down event is fired, and that changes that player's x and y positions, moving the player div around the screen.

--- src/components/World1/World1.jsx
+++ src/components/World1/World1.jsx
@@ -1,20 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './World1.module.css';
 
-const World1 = () => (
-	<div className={styles.World1Container}>
-		<NavLink 
-		exact 
-		to='/harvest' 
-		className={styles.BackToHarvestPage} 
-		>
-			<span className={styles.Close}>&times;</span>
-		</NavLink>
-		<div className={styles.World1}>
-			
-		</div>
-	</div>
-)
+class World1 extends Component {
+
+	state ={
+		playerX: 0,
+		playerY: 0
+	}
+
+	componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyPress);
+  }
+
+	componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyPress);
+	}
+	
+	handleKeyPress = (e) => {
+    switch(e.keyCode) {
+      case 37:
+      this.setState({
+        playerX: this.state.playerX - 2
+      });
+      break;
+      case 39:
+      this.setState({
+        playerX: this.state.playerX + 2
+      });
+      break;
+      case 38:
+      this.setState({
+        playerY: this.state.playerY - 2
+      });
+      break;
+      case 40:
+      this.setState({
+        playerY: this.state.playerY + 2
+      });
+      break;
+      default:
+    }
+  }
+
+	render() {
+
+		let playerPos = {
+			left: `${this.state.playerX}%`,
+			top: `${this.state.playerY}%`,
+		}
+
+		return (
+			<div className={styles.World1Container}>
+				<NavLink 
+				exact 
+				to='/harvest' 
+				className={styles.BackToHarvestPage} 
+				>
+					<span className={styles.Close}>&times;</span>
+				</NavLink>
+				<div className={styles.World1}>
+					<div className={styles.PlayerBlock} style={playerPos}></div>
+				</div>
+			</div>
+		);
+	}
+}
 
 export default World1;
